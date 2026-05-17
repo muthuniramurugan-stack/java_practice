4e871500217da7f081ad342c2cfb069d1e69c7e5
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-05-17 08:16:40 +0530

    added

---
+++ NumberToWord.java
@@ -0,0 +1,41 @@
+public class NumberToWord {
+    public static void main(String[] args) {
+        int num = 123;
+        String str = num + "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            switch (ch) {
+                case '0':
+                    System.out.print("Zero ");
+                    break;
+                case '1':
+                    System.out.print("One ");
+                    break;
+                case '2':
+                    System.out.print("Two ");
+                    break;
+                case '3':
+                    System.out.print("Three ");
+                    break;
+                case '4':
+                    System.out.print("Four ");
+                    break;
+                case '5':
+                    System.out.print("Five ");
+                    break;
+                case '6':
+                    System.out.print("Six ");
+                    break;
+                case '7':
+                    System.out.print("Seven ");
+                    break;
+                case '8':
+                    System.out.print("Eight ");
+                    break;
+                case '9':
+                    System.out.print("Nine ");
+                    break;
+            }
+        }
+    }
+}
--- middledigit.java
+++ middledigit.java
@@ -1,5 +1,10 @@
 public class middledigit {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         int num = 12345;
+        String n = num + "";
+        int start = 0;
+        int end = n.length() - 1;
+        int mid = start + (end - start) / 2;
+        System.out.println(n.charAt(mid));
     }
 }
