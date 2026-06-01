3f16823f16fcba2ea772ef1f590efa41311fa6dd
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-01 17:52:02 +0530

    added

---
+++ Strings/countvowels.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class countvowels {
+    public static void main(String[] args) {
+        String name = "muthumurugan";
+        int count =0;
+        for (int i = 0; i < name.length(); i++) {
+            char ch = name.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
