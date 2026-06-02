9eaa47d6592d4dd1af9ee4381e134dda91a49e4e
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-02 09:06:51 +0530

    added

---
+++ Strings/removespaces.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class removespaces {
+    public static void main(String[]args){
+        String name = "Hello How Are You";
+        String result = "";
+        for(int i=0;i<name.length();i++){
+            char ch = name.charAt(i);
+            if(ch!=' '){
+                result+=ch;
+            }
+        }
+        System.out.println(result);
+    }
+}
