329dc3fa1eb8e44de15f9b0616899594e65c785b
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-02 15:19:20 +0530

    added

---
+++ Strings/ascii.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class ascii {
+    public static void main(String[]args){
+        char ch = 'A';
+        int ascii = (int)ch;
+        System.out.println(ascii);
+    }
+}
