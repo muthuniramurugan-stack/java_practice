9a4fa93cf9964e111736dffb428e4f8bbc2e4d12
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-02 15:14:06 +0530

    added

---
+++ Strings/length.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class length {
+    public static void main(String[] args) {
+        String name = "Muthumurugan";
+        int count =0;
+        try{
+            while(true){
+                name.charAt(count);
+                count++;
+            }
+        }catch(Exception e){
+            System.out.println(count);
+        }
+    
+    }
+}
--- Strings/printcharacter.java
+++ Strings/printcharacter.java
@@ -1,11 +1,11 @@
 package Strings;
 
 public class printcharacter {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         String name = "hello";
-        for(int i=0;i<name.length();i++){
+        for (int i = 0; i < name.length(); i++) {
             char ch = name.charAt(i);
             System.out.println(ch);
-        }    
+        }
     }
 }
