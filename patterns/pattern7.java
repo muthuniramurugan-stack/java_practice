9cf6a3c430d2062df77433554289d56940934fcb
muthuniramurugan-stack <muthuniramurugan@gmail.com>
2026-06-22 19:52:40 +0530

    added

---
+++ patterns/pattern7.java
@@ -0,0 +1,33 @@
+/*
+
+*****
+****
+***
+**
+*
+**
+***
+****
+*****
+
+
+*/
+package patterns;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
